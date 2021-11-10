a2aa8f04c6621558203a431e38a39732ab171842
kaanb93 <52814388+kaanb93@users.noreply.github.com>
2021-11-10 09:29:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="q2-krona",
-    version='0.0.0.1',
+    version='1.0.0',
     packages=find_packages(),
     author="Kaan Büyükaltay",
     author_email="kaanb93@gmail.com",
